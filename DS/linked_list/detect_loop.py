1bcc3695f726837d4063a2ca5439677191a85f20
hari311007 <hn311020007@gmail.com>
2026-08-02 19:10:25 +0530

    added successfully

---
+++ DS/linked_list/detect_loop.py
@@ -0,0 +1,11 @@
+class Solution:
+    def detectLoop(self, head):
+        fast=head
+        slow=head
+        while(fast is not None and fast.next is not None):
+            fast=fast.next.next
+            slow=slow.next
+            if(fast==slow):
+                return True
+        return False
+        
